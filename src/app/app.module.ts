fbaaf8d265e3571c2225cedb2056ebafb458608f
Sebastian Uddén <sebastian.udden@eurostep.com>
2017-11-17 00:52:28 +0100

    Responsive homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    FooterComponent,
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
